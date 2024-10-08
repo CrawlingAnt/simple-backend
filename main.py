92e0da4925e3a5359a32b2659a2300ac16befaff
ylm <1044190862@qq.com>
2024-10-08 19:38:40 +0800

    [new]  改用sqlmodel

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 import user.urls as user_urls
 import article.urls as article_urls
-import settings.orm as init_db
 import uvicorn
 from middleware.blackList import BlackListMiddleware
 
@@ -16,5 +15,4 @@ app.include_router(user_urls.router)
 app.include_router(article_urls.router)
 
 if __name__ == "__main__":
-    init_db()
     uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
