f9e510804d88ac4b1d67f161c3f87a132ac43ab7
leon <>
2024-10-09 00:12:52 +0800

    orm

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import user.urls as user_urls
 import article.urls as article_urls
 import uvicorn
 from middleware.blackList import BlackListMiddleware
-
+from settings.orm import init_db
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='assets'), name='static')
 
@@ -15,4 +15,5 @@ app.include_router(user_urls.router)
 app.include_router(article_urls.router)
 
 if __name__ == "__main__":
+    init_db()
     uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
--- settings/orm.py
+++ settings/orm.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine,SQLModel,Session
+from sqlalchemy import create_engine
 
-engine = create_engine("mysql://root:a198man204@127.0.0.1:3306/study_system")
+engine = create_engine("mysql+pymysql://root:a198man204@127.0.0.1:3306/study_system")
 
 def init_db():
-    SQLModel.metadata.create_all(engine, echo=True)
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
