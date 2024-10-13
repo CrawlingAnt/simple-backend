058c2a6738b2510a514377b3deca878fadd7daf8
leon <>
2024-10-13 12:01:24 +0800

    异步数据添加

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,7 +7,7 @@ from models.main import User
 from common.constant import *
 from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
-import asyncio
+
 
 router = APIRouter(
     prefix="/user",
@@ -26,21 +26,16 @@ async def all_students(user: QueryUser):
 
 
 @router.post("/add")
-async def test(users: AddUser, session: AsyncSession = Depends(get_async_session)):
-    start = datetime.now()
-    tasks = []
-    async with session.begin():
-        # 检查用户是否存在等逻辑
-        tasks.append(asyncio.create_task(process_user(users, session)))
-        await asyncio.gather(*tasks)
-        return {"message": f"{ start} - {datetime.now()} Users created successfully"}
-
-async def process_user(user: AddUser, session: AsyncSession):
-    is_exists = (await session.execute(select(User).where(User.user_name == user.user_name))).scalar_one_or_none()
+async def test(user: AddUser, session: AsyncSession = Depends(get_async_session)):
+    is_exists = await session.execute(select(User).where(User.user_name == user.user_name))
+    is_exists = is_exists.scalar_one_or_none()
     if is_exists:
         raise HTTPException(status_code=400, detail=USER_EXISTS)
-    hashed_password = get_password_hash(user.password)
-    print(hashed_password)
-    new_user = User(user_name=user.user_name, hashed_password=hashed_password, password=user.password)
-    session.add(new_user)
-
+    new_user = User(user_name=user.user_name, hashed_password=get_password_hash(user.password), password=user.password)
+    try:
+        session.add(new_user)
+        await session.commit()
+        return {"message": "User created successfully"}
+    except Exception as e:
+        await session.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
