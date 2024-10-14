75470ad0149a88bc8ca35616ae36aad16fccf097
ylm <1044190862@qq.com>
2024-10-14 16:42:10 +0800

    [new]   加入日志

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ import article.urls as article_urls
 import uvicorn
 from middleware import init_middleware
 from common.global_catch import init_global_exception_handler
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='assets'), name='static')
+
 
 init_global_exception_handler(app)
 # 加载中间件
@@ -17,4 +21,4 @@ app.include_router(user_urls.router)
 app.include_router(article_urls.router)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(os.getenv("APP_NAME"), host=os.getenv("HOST"), port=int(os.getenv("PORT")), reload=os.getenv("RELOAD") == "True")
